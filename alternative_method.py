7e800258120a1f496b54d00234908c580cc8c2e5
Lakshay_Edicts <77452480+LakshayEdicts@users.noreply.github.com>
2021-03-30 07:25:59 +0530

    Create alternative_method.py

---
+++ alternative_method.py
@@ -0,0 +1,10 @@
+ask  = input("What the time right now")
+lights_on = True
+lights_off = True
+if ask == "Daytime":
+    print("Lights Off")
+    print(lights_off)
+else:
+    if ask == "Night Time":
+        print("Light On")
+        print(lights_on)
